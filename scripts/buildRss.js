0a151887e9aefe2d289de2390614f419a55354a5
Mark Shlick <markshlick@gmail.com>
2020-05-29 09:00:40 -0700

    x

--- scripts/buildRss.js
+++ scripts/buildRss.js
@@ -3,10 +3,10 @@ const rssFeedFilePath = 'scripts/rss.ts';
 function go() {
   let generateRss;
   try {
-    generateRss = require(`./.next/server/${rssFeedFilePath}`).default;
+    generateRss = require(`../.next/server/${rssFeedFilePath}`).default;
   } catch (error) {
     // vercel uses the serverless dir
-    generateRss = require(`./.next/serverless/${rssFeedFilePath}`).default;
+    generateRss = require(`../.next/serverless/${rssFeedFilePath}`).default;
   }
 
   return generateRss();

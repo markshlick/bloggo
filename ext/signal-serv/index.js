a8df8bd1e5a0c600959b549a1a6bd927e136b8a3
Mark Shlick <markshlick@gmail.com>
2020-06-17 17:38:05 -0700

    x

--- ext/signal-serv/index.js
+++ ext/signal-serv/index.js
@@ -9,14 +9,14 @@ const app = express();
 const server = http.createServer(app);
 const io = IO(server);
 
-if (process.env.NODE_ENV === 'development') {
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-}
+const isDev = process.env.NODE_ENV === 'development';
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', isDev ? '*' : 'https://mksh.io');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200).end();
